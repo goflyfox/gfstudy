46d3e2314453b17b861154c8eea1fe0ea14e444e
zcool321@sina.com <1234qwer>
2020-03-22 23:56:06 +0800

    update redis example

---
+++ 08.database/go.mod
@@ -0,0 +1,5 @@
+module gf_database
+
+go 1.14
+
+require github.com/gogf/gf v1.11.7
---
+++ 08.database/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gogf/gf/frame/g"
+)
+
+func main() {
+	// 对应默认配置项 logger，默认default
+	g.Log().Debug("[default]Debug")
+}
