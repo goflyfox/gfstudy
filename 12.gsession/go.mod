e7c3fc26908d8b3b1c96274cc9b521b78b3752b6
zcool321@sina.com <1234qwer>
2020-04-12 00:42:13 +0800

    add gsession example

---
+++ 12.gsession/go.mod
@@ -0,0 +1,5 @@
+module gf-gsession
+
+go 1.14
+
+require github.com/gogf/gf v1.12.1
---
+++ 12.gsession/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+	"github.com/gogf/gf/os/gsession"
+)
+
+const SessionUser = "SessionUser"
+
+func main() {
+	s := g.Server()
+
+	// 设置存储方式
+	sessionStorage := g.Config().GetString("SessionStorage")
+	if sessionStorage == "redis" {
+		s.SetConfigWithMap(g.Map{
+			"SessionStorage": gsession.NewStorageRedis(g.Redis()),
+		})
+	} else if sessionStorage == "memory" {
+		s.SetConfigWithMap(g.Map{
+			"SessionStorage": gsession.NewStorageMemory(),
+		})
+		gsession.NewStorageFile("/tmp")
+	}
+
+	// 常规注册
+	group := s.Group("/")
+	group.GET("/", func(r *ghttp.Request) {
+		r.Response.WriteTpl("index.html", g.Map{
+			"title": "登录页面",
+		})
+	})
+	group.POST("/login", func(r *ghttp.Request) {
+		username := r.GetString("username")
+		password := r.GetString("password")
+
+		//dbUsername := "admin"
+		//dbPassword := "123456"
+		dbUsername := g.Config().GetString("username")
+		dbPassword := g.Config().GetString("password")
+		if username == dbUsername && password == dbPassword {
+			// 添加session
+			r.Session.Set(SessionUser, g.Map{
+				"username": dbUsername,
+				"name":     "管理员",
+			})
+			r.Response.WriteJson(g.Map{
+				"code": 0,
+				"msg":  "登录成功",
+			})
+			r.Exit()
+		}
+
+		r.Response.WriteJson(g.Map{
+			"code": -1,
+			"msg":  "登录失败",
+		})
+	})
+
+	// 用户组
+	userGroup := s.Group("/user")
+	userGroup.Middleware(MiddlewareAuth)
+	userGroup.GET("/index", func(r *ghttp.Request) {
+		r.Response.WriteTpl("user_index.html", g.Map{
+			"title": "登录页面",
+		})
+	})
+	userGroup.POST("/list", func(r *ghttp.Request) {
+		r.Response.WriteJson(g.Map{
+			"code": 0,
+			"msg":  "成功",
+			"data": g.List{
+				g.Map{
+					"date":    "2020-04-01",
+					"name":    "朱元璋",
+					"address": "江苏110号",
+				},
+				g.Map{
+					"date":    "2020-04-02",
+					"name":    "徐达",
+					"address": "江苏111号",
+				},
+				g.Map{
+					"date":    "2020-04-03",
+					"name":    "李善长",
+					"address": "江苏112号",
+				},
+			}})
+	})
+	userGroup.POST("/logout", func(r *ghttp.Request) {
+		// 删除session
+		r.Session.Remove(SessionUser)
+
+		r.Response.WriteJson(g.Map{
+			"code": 0,
+			"msg":  "登出成功",
+		})
+	})
+
+	s.SetPort(8199)
+	s.Run()
+}
+
+// 认证中间件
+func MiddlewareAuth(r *ghttp.Request) {
+	if r.Session.Contains(SessionUser) {
+		r.Middleware.Next()
+	} else {
+		// 获取用错误码
+		r.Response.WriteJson(g.Map{
+			"code": 403,
+			"msg":  "您访问超时或已登出",
+		})
+	}
+}
