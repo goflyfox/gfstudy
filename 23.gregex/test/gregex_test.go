c0348555de8d108601a54040a1770657606246be
jflyfox <zcool321@sina.com>
2020-09-06 00:26:53 +0800

    update gregex

--- 23.gregex/test/gregex_test.go
+++ 23.gregex/test/gregex_test.go
@@ -2,297 +2,100 @@ package test
 
 import (
 	"fmt"
-	"github.com/gogf/gf/test/gtest"
 	"github.com/gogf/gf/text/gregex"
-	"strings"
 	"testing"
 )
 
 // IsMatch
 func TestIsMatch(t *testing.T) {
-	var pattern = `(.+):(\d+)`
-	s1 := []byte(`sfs:2323`)
-	fmt.Println("IsMatch", gregex.IsMatch(pattern, s1))
+	// 校验时间是否合法
+	var pattern = `\d{4}-\d{2}-\d{2}`
+	s1 := []byte(`2019-07-20`)
+	fmt.Println("IsMatch1", gregex.IsMatch(pattern, s1))
+	pattern = `[21]\d{3}-\d{1,2}-\d{1,2}`
+	fmt.Println("IsMatch2", gregex.IsMatch(pattern, s1))
 }
 
 // IsMatchString
 func TestIsMatchString(t *testing.T) {
-	var pattern = `(.+):(\d+)`
-	s1 := `sfs:2323`
+	var pattern = `[21]\d{3}-[01]?\d-[0123]?\d`
+	s1 := `2019-07-20`
 	fmt.Println("IsMatchString", gregex.IsMatchString(pattern, s1))
 }
 
 var (
-	PatternErr = `([\d+`
+	textStr     = "123 xiangyu liubang xiangyu liubang"
+	patternStr  = `\d+\s(\w+)\s\w+\s\w+\s\w+`
+	patternStr2 = `\d+\s(\w+)`
+	patternStr3 = `(\w+)\sliubang`
 )
 
 // Match
 func TestMatch(t *testing.T) {
-	re := "a(a+b+)b"
-	wantSubs := "aaabb"
-	s := "acbb" + wantSubs + "dd"
-	subs, err := gregex.Match(re, []byte(s))
+	subs, err := gregex.Match(patternStr, []byte(textStr))
 	if err != nil {
 		t.Error("Match", err)
 	}
-	fmt.Println("Match", string(subs[0]),string(subs[1]), err)
+	fmt.Println("Match", string(subs[0]), "##group:", string(subs[1]), err)
 }
 
-func Test_MatchString(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		wantSubs := "aaabb"
-		s := "acbb" + wantSubs + "dd"
-		subs, err := gregex.MatchString(re, s)
-		t.Assert(err, nil)
-		if string(subs[0]) != wantSubs {
-			t.Fatalf("regex:%s,Match(%q)[0] = %q; want %q", re, s, subs[0], wantSubs)
-		}
-		if string(subs[1]) != "aab" {
-			t.Fatalf("Match(%q)[1] = %q; want %q", s, subs[1], "aab")
-		}
-		// error pattern
-		_, err = gregex.MatchString(PatternErr, s)
-		t.AssertNE(err, nil)
-	})
+// MatchString
+func TestMatchString(t *testing.T) {
+	// 匹配全部内容
+	subs, err := gregex.MatchString(patternStr, textStr)
+	if err != nil {
+		t.Error("MatchString", err)
+	}
+	fmt.Println("MatchString", subs[0], "##group:", subs[1], err)
+
+	// 匹配部分内容
+	subs, err = gregex.MatchString(patternStr2, textStr)
+	if err != nil {
+		t.Error("MatchString2", err)
+	}
+	fmt.Println("MatchString2", subs[0], "##group:", subs[1], err)
 }
 
-func Test_MatchAll(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		wantSubs := "aaabb"
-		s := "acbb" + wantSubs + "dd"
-		s = s + `其他的` + s
-		subs, err := gregex.MatchAll(re, []byte(s))
-		t.Assert(err, nil)
-		if string(subs[0][0]) != wantSubs {
-			t.Fatalf("regex:%s,Match(%q)[0] = %q; want %q", re, s, subs[0][0], wantSubs)
-		}
-		if string(subs[0][1]) != "aab" {
-			t.Fatalf("Match(%q)[1] = %q; want %q", s, subs[0][1], "aab")
-		}
-
-		if string(subs[1][0]) != wantSubs {
-			t.Fatalf("regex:%s,Match(%q)[0] = %q; want %q", re, s, subs[1][0], wantSubs)
-		}
-		if string(subs[1][1]) != "aab" {
-			t.Fatalf("Match(%q)[1] = %q; want %q", s, subs[1][1], "aab")
-		}
-		// error pattern
-		_, err = gregex.MatchAll(PatternErr, []byte(s))
-		t.AssertNE(err, nil)
-	})
+// MatchAll
+func TestMatchAll(t *testing.T) {
+	allGroup, err := gregex.MatchAll(patternStr3, []byte(textStr))
+	if err != nil {
+		t.Error("MatchAll", err)
+	}
+	fmt.Println("MatchAll", string(allGroup[0][0]), "##group:", string(allGroup[0][1]), err)
+	fmt.Println("MatchAll", string(allGroup[1][0]), "##group:", string(allGroup[1][1]), err)
 }
 
-func Test_MatchAllString(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		wantSubs := "aaabb"
-		s := "acbb" + wantSubs + "dd"
-		subs, err := gregex.MatchAllString(re, s+`其他的`+s)
-		t.Assert(err, nil)
-		if string(subs[0][0]) != wantSubs {
-			t.Fatalf("regex:%s,Match(%q)[0] = %q; want %q", re, s, subs[0][0], wantSubs)
-		}
-		if string(subs[0][1]) != "aab" {
-			t.Fatalf("Match(%q)[1] = %q; want %q", s, subs[0][1], "aab")
-		}
-
-		if string(subs[1][0]) != wantSubs {
-			t.Fatalf("regex:%s,Match(%q)[0] = %q; want %q", re, s, subs[1][0], wantSubs)
-		}
-		if string(subs[1][1]) != "aab" {
-			t.Fatalf("Match(%q)[1] = %q; want %q", s, subs[1][1], "aab")
-		}
-		// error pattern
-		_, err = gregex.MatchAllString(PatternErr, s)
-		t.AssertNE(err, nil)
-	})
+// MatchAllString
+func TestMatchAllString(t *testing.T) {
+	allGroup, err := gregex.MatchAllString(patternStr3, textStr)
+	if err != nil {
+		t.Error("MatchAllString", err)
+	}
+	fmt.Println("MatchAllString", allGroup, "##group:", allGroup[0][1], err)
 }
 
-func Test_Replace(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		wantSubs := "aaabb"
-		replace := "12345"
-		s := "acbb" + wantSubs + "dd"
-		wanted := "acbb" + replace + "dd"
-		replacedStr, err := gregex.Replace(re, []byte(replace), []byte(s))
-		t.Assert(err, nil)
-		if string(replacedStr) != wanted {
-			t.Fatalf("regex:%s,old:%s; want %q", re, s, wanted)
-		}
-		// error pattern
-		_, err = gregex.Replace(PatternErr, []byte(replace), []byte(s))
-		t.AssertNE(err, nil)
-	})
+// Replace
+func TestReplace(t *testing.T) {
+	replace, err := gregex.Replace(patternStr3, []byte("zhuyuanzhang chenyouliang"), []byte(textStr))
+	if err != nil {
+		t.Error("Replace", err)
+	}
+	fmt.Println("Replace", string(replace), "##src:", textStr, err)
+
 }
 
-func Test_ReplaceString(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		wantSubs := "aaabb"
-		replace := "12345"
-		s := "acbb" + wantSubs + "dd"
-		wanted := "acbb" + replace + "dd"
-		replacedStr, err := gregex.ReplaceString(re, replace, s)
-		t.Assert(err, nil)
-		if replacedStr != wanted {
-			t.Fatalf("regex:%s,old:%s; want %q", re, s, wanted)
-		}
-		// error pattern
-		_, err = gregex.ReplaceString(PatternErr, replace, s)
-		t.AssertNE(err, nil)
-	})
+// ReplaceString
+func TestReplaceString(t *testing.T) {
+	replacedStr, err := gregex.ReplaceString(patternStr3, "zhuyuanzhang chenyouliang", textStr)
+	if err != nil {
+		t.Error("ReplaceString", err)
+	}
+	fmt.Println("ReplaceString", replacedStr, "##src:", textStr, err)
 }
 
-func Test_ReplaceFun(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		wantSubs := "aaabb"
-		//replace :="12345"
-		s := "acbb" + wantSubs + "dd"
-		wanted := "acbb[x" + wantSubs + "y]dd"
-		wanted = "acbb" + "3个a" + "dd"
-		replacedStr, err := gregex.ReplaceFunc(re, []byte(s), func(s []byte) []byte {
-			if strings.Index(string(s), "aaa") >= 0 {
-				return []byte("3个a")
-			}
-			return []byte("[x" + string(s) + "y]")
-		})
-		t.Assert(err, nil)
-		if string(replacedStr) != wanted {
-			t.Fatalf("regex:%s,old:%s; want %q", re, s, wanted)
-		}
-		// error pattern
-		_, err = gregex.ReplaceFunc(PatternErr, []byte(s), func(s []byte) []byte {
-			return []byte("")
-		})
-		t.AssertNE(err, nil)
-	})
-}
-
-func Test_ReplaceFuncMatch(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		s := []byte("1234567890")
-		p := `(\d{3})(\d{3})(.+)`
-		s0, e0 := gregex.ReplaceFuncMatch(p, s, func(match [][]byte) []byte {
-			return match[0]
-		})
-		t.Assert(e0, nil)
-		t.Assert(s0, s)
-		s1, e1 := gregex.ReplaceFuncMatch(p, s, func(match [][]byte) []byte {
-			return match[1]
-		})
-		t.Assert(e1, nil)
-		t.Assert(s1, []byte("123"))
-		s2, e2 := gregex.ReplaceFuncMatch(p, s, func(match [][]byte) []byte {
-			return match[2]
-		})
-		t.Assert(e2, nil)
-		t.Assert(s2, []byte("456"))
-		s3, e3 := gregex.ReplaceFuncMatch(p, s, func(match [][]byte) []byte {
-			return match[3]
-		})
-		t.Assert(e3, nil)
-		t.Assert(s3, []byte("7890"))
-		// error pattern
-		_, err := gregex.ReplaceFuncMatch(PatternErr, s, func(match [][]byte) []byte {
-			return match[3]
-		})
-		t.AssertNE(err, nil)
-	})
-}
-
-func Test_ReplaceStringFunc(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		wantSubs := "aaabb"
-		//replace :="12345"
-		s := "acbb" + wantSubs + "dd"
-		wanted := "acbb[x" + wantSubs + "y]dd"
-		wanted = "acbb" + "3个a" + "dd"
-		replacedStr, err := gregex.ReplaceStringFunc(re, s, func(s string) string {
-			if strings.Index(s, "aaa") >= 0 {
-				return "3个a"
-			}
-			return "[x" + s + "y]"
-		})
-		t.Assert(err, nil)
-		if replacedStr != wanted {
-			t.Fatalf("regex:%s,old:%s; want %q", re, s, wanted)
-		}
-		// error pattern
-		_, err = gregex.ReplaceStringFunc(PatternErr, s, func(s string) string {
-			return ""
-		})
-		t.AssertNE(err, nil)
-	})
-}
-
-func Test_ReplaceStringFuncMatch(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		s := "1234567890"
-		p := `(\d{3})(\d{3})(.+)`
-		s0, e0 := gregex.ReplaceStringFuncMatch(p, s, func(match []string) string {
-			return match[0]
-		})
-		t.Assert(e0, nil)
-		t.Assert(s0, s)
-		s1, e1 := gregex.ReplaceStringFuncMatch(p, s, func(match []string) string {
-			return match[1]
-		})
-		t.Assert(e1, nil)
-		t.Assert(s1, "123")
-		s2, e2 := gregex.ReplaceStringFuncMatch(p, s, func(match []string) string {
-			return match[2]
-		})
-		t.Assert(e2, nil)
-		t.Assert(s2, "456")
-		s3, e3 := gregex.ReplaceStringFuncMatch(p, s, func(match []string) string {
-			return match[3]
-		})
-		t.Assert(e3, nil)
-		t.Assert(s3, "7890")
-		// error pattern
-		_, err := gregex.ReplaceStringFuncMatch(PatternErr, s, func(match []string) string {
-			return ""
-		})
-		t.AssertNE(err, nil)
-	})
-}
-
-func Test_Split(t *testing.T) {
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		matched := "aaabb"
-		item0 := "acbb"
-		item1 := "dd"
-		s := item0 + matched + item1
-		t.Assert(gregex.IsMatchString(re, matched), true)
-		items := gregex.Split(re, s) //split string with matched
-		if items[0] != item0 {
-			t.Fatalf("regex:%s,Split(%q) want %q", re, s, item0)
-		}
-		if items[1] != item1 {
-			t.Fatalf("regex:%s,Split(%q) want %q", re, s, item0)
-		}
-	})
-
-	gtest.C(t, func(t *gtest.T) {
-		re := "a(a+b+)b"
-		notmatched := "aaxbb"
-		item0 := "acbb"
-		item1 := "dd"
-		s := item0 + notmatched + item1
-		t.Assert(gregex.IsMatchString(re, notmatched), false)
-		items := gregex.Split(re, s) //split string with notmatched then nosplitting
-		if items[0] != s {
-			t.Fatalf("regex:%s,Split(%q) want %q", re, s, item0)
-		}
-		// error pattern
-		items = gregex.Split(PatternErr, s)
-		t.AssertEQ(items, nil)
-
-	})
+// Split
+func TestSplit(t *testing.T) {
+	items := gregex.Split(`\sxiangyu\s`, textStr)
+	fmt.Println("Split", items, "###0:", items[0], "##src:", textStr)
 }
