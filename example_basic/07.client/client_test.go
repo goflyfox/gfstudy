7fbd463f8b471157ead302e10211811a54a3f094
zcool321 <zcool321@sina.com>
2025-07-08 23:20:44 +0800

    docs(client): 更新 GoFrame HTTP客户端文档和示例- 重新组织文档结构，增加常用 HTTP 客户端工具介绍 - 更新 GoFrame HTTP 客户端使用示例，包括 GET、POST 请求等 - 添加中间件使用示例 - 更新相关测试代码

---
+++ example_basic/07.client/client_test.go
@@ -0,0 +1,110 @@
+package main_test
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/gclient"
+	"github.com/gogf/gf/v2/os/gctx"
+	"net/http"
+	"testing"
+)
+
+var path = "http://127.0.0.1:8199/api"
+
+// GET/POST请求
+func TestWelcome(t *testing.T) {
+	ctx := gctx.New()
+	if response, err := g.Client().Get(ctx, path); err != nil {
+		panic(err)
+	} else {
+		defer response.Close()
+		t.Log(response.RawRequest())
+		t.Log(response.RawResponse())
+		t.Log(response.ReadAllString())
+	}
+	if response, err := g.Client().Post(ctx, path); err != nil {
+		panic(err)
+	} else {
+		defer response.Close()
+		t.Log(response.Raw()) // 包含请求和响应
+		t.Log(response.ReadAllString())
+	}
+}
+
+// GET请求带参数
+func TestGet(t *testing.T) {
+	if response, err := g.Client().Get(gctx.New(), path+"/get?name=whoami"); err != nil {
+		panic(err)
+	} else {
+		defer response.Close()
+		response.RawDump()
+	}
+}
+
+// POST请求
+func TestPost(t *testing.T) {
+	if response, err := g.Client().Post(gctx.New(), path+"/post", "name=fox&age=18"); err != nil {
+		panic(err)
+	} else {
+		defer response.Close()
+		response.RawDump()
+	}
+}
+
+// POST JSON
+func TestPostJson(t *testing.T) {
+	if response, err := g.Client().Post(gctx.New(), path+"/post/json",
+		g.Map{"passport": "fox", "password": "fox123"}); err != nil {
+		panic(err)
+	} else {
+		defer response.Close()
+		response.RawDump()
+	}
+}
+
+// POST Header头
+func TestPostHeader(t *testing.T) {
+	c := g.Client()
+	c.SetHeader("Cookie", "name=fox; score=100")
+	//c.SetCookieMap(g.MapStrStr{
+	//	"name":  "fox",
+	//	"score": "100",
+	//})
+	if r, e := c.Post(gctx.New(), path+"/post/header"); e != nil {
+		panic(e)
+	} else {
+		r.RawDump()
+	}
+}
+
+// POST Header头
+func TestPostHeaderRaw(t *testing.T) {
+	c := g.Client()
+	c.SetHeaderRaw(`
+accept-encoding: gzip, deflate, br
+accept-language: zh-CN,zh;q=0.9,en;q=0.8
+referer: https://idonottell.you
+cookie: name=fox; score=100
+user-agent: my test http client
+ 	`)
+	if r, e := c.Post(gctx.New(), path+"/post/header/raw"); e != nil {
+		panic(e)
+	} else {
+		r.RawDump()
+	}
+}
+
+// Test Handler
+func TestHandler(t *testing.T) {
+	c := g.Client()
+	c.Use(func(c *gclient.Client, r *http.Request) (resp *gclient.Response, err error) {
+		t.Log("Handler前")
+		resp, err = c.Next(r)
+		t.Log("Handler后")
+		return resp, err
+	})
+	if r, e := c.Post(gctx.New(), path); e != nil {
+		panic(e)
+	} else {
+		r.RawDump()
+	}
+}
--- example_basic/07.client/main.go
+++ example_basic/07.client/main.go
@@ -7,44 +7,45 @@ import (
 
 func main() {
 	s := g.Server()
-	group := s.Group("/api")
-	// 默认路径
-	group.ALL("/", func(r *ghttp.Request) {
-		r.Response.Writeln("Welcome GoFrame!")
-	})
-	// GET带参数
-	group.GET("/hello", func(r *ghttp.Request) {
-		r.Response.Writeln("Hello World!")
-		r.Response.Writeln("name:", r.Get("name").String())
-	})
-	// POST KV
-	group.POST("/test", func(r *ghttp.Request) {
-		r.Response.Writeln("func:test")
-		r.Response.Writeln("name:", r.Get("name").String())
-		r.Response.Writeln("age:", r.Get("age").Int())
-	})
-	// POST JSON
-	group.POST("/test2", func(r *ghttp.Request) {
-		r.Response.Writeln("func:test2")
-		r.Response.Writeln("passport:", r.Get("passport").String())
-		r.Response.Writeln("password:", r.Get("password").String())
-	})
-	// POST Header
-	group.POST("/test3", func(r *ghttp.Request) {
-		r.Response.Writeln("func:test3")
-		r.Response.Writeln("Cookie:", r.Header.Get("Cookie"))
-	})
-	// POST Header
-	group.POST("/test4", func(r *ghttp.Request) {
-		r.Response.Writeln("func:test4")
-		h := r.Header
-		r.Response.Writeln("accept-encoding:", h.Get("accept-encoding"))
-		r.Response.Writeln("accept-language:", h.Get("accept-language"))
-		r.Response.Writeln("referer:", h.Get("referer"))
-		r.Response.Writeln("cookie:", h.Get("cookie"))
-		r.Response.Writeln(r.Cookie.Map())
+	s.Group("/api", func(group *ghttp.RouterGroup) {
+		// 默认路径
+		group.ALL("/", func(r *ghttp.Request) {
+			r.Response.Writeln("Welcome GoFrame!")
+		})
+		// GET带参数
+		group.GET("/get", func(r *ghttp.Request) {
+			r.Response.Writeln("func:/get")
+			r.Response.Writeln("Hello World!")
+			r.Response.Writeln("name:", r.Get("name").String())
+		})
+		// POST KV
+		group.POST("/post", func(r *ghttp.Request) {
+			r.Response.Writeln("func:/post")
+			r.Response.Writeln("name:", r.Get("name").String())
+			r.Response.Writeln("age:", r.Get("age").Int())
+		})
+		// POST JSON
+		group.POST("/post/json", func(r *ghttp.Request) {
+			r.Response.Writeln("func:/post/json")
+			r.Response.Writeln("passport:", r.Get("passport").String())
+			r.Response.Writeln("password:", r.Get("password").String())
+		})
+		// POST Header
+		group.POST("/post/header", func(r *ghttp.Request) {
+			r.Response.Writeln("func:/post/header")
+			r.Response.Writeln("Cookie:", r.Header.Get("Cookie"))
+		})
+		// POST Header
+		group.POST("/post/header/raw", func(r *ghttp.Request) {
+			r.Response.Writeln("func:/post/header/raw")
+			h := r.Header
+			r.Response.Writeln("accept-encoding:", h.Get("accept-encoding"))
+			r.Response.Writeln("accept-language:", h.Get("accept-language"))
+			r.Response.Writeln("referer:", h.Get("referer"))
+			r.Response.Writeln("cookie:", h.Get("cookie"))
+			r.Response.Writeln(r.Cookie.Map())
+		})
 	})
 
-	s.SetPort(8199)
 	s.Run()
 }
